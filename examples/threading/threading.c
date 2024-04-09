8b50c61391205efd5939821a931fe8f60e41f98d
mohammedaslam5 <mohammed.aslam5@cognizant.com>
2024-04-09 07:27:38 +0000

    assignment-4-part-1 first commit

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,10 +13,40 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int rc;
+
+    DEBUG_LOG("Waiting to obtain mutex");
+    sleep(thread_func_args->wait_to_obtain_ms/1000);
+
+    rc = pthread_mutex_lock(thread_func_args->mutex);
+    if( rc != 0 )
+    {
+	    ERROR_LOG("Failed to obtain mutex");
+	    thread_func_args->thread_complete_success = false;
+    }
+    else
+    {
+	    DEBUG_LOG("Obtained mutex successfully");
+
+	    DEBUG_LOG("Waiting to release mutex");
+	    sleep(thread_func_args->wait_to_release_ms/1000);
+
+	    rc = pthread_mutex_unlock(thread_func_args->mutex);
+	    if( rc != 0 )
+	    {
+		    ERROR_LOG("Failed to release mutex");
+		    thread_func_args->thread_complete_success = false;
+	    }
+	    else
+	    {
+		    DEBUG_LOG("Released mutex successfully");
+		    thread_func_args->thread_complete_success = true;
+	    }
+    }
+
     return thread_param;
 }
-
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
@@ -28,6 +58,35 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+
+    struct thread_data* thread_param = NULL;
+    int rc;
+    
+
+    thread_param = malloc(sizeof(struct thread_data));
+    if(thread_param == NULL) {
+	    ERROR_LOG("malloc() for thread_data failed");
+	    return false;
+    }
+
+//    rc = pthread_mutex_init(mutex, NULL);
+//    if( rc != 0) {
+//	    ERROR_LOG("pthread_mutex_init() failed");
+//    }
+    
+    thread_param->mutex = mutex;
+    thread_param->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_param->wait_to_release_ms = wait_to_release_ms;
+
+    rc = pthread_create(thread, NULL, threadfunc, thread_param);
+    if( rc != 0 ) {
+            ERROR_LOG("pthread_create() failed");
+	    return false;
+    }
+
+    DEBUG_LOG("Created thread successfully, ID = %d", thread);
+
+    return true;
 }
 
